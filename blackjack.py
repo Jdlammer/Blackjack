8363ce3ba8acffff594207f1e4ba1dd063d71e71
Justice Lammers <justicelammers11@gmail.com>
2018-06-19 23:38:06 -0700

    Halfway done with play class. Need to finish adding hit/stay option

--- blackjack.py
+++ blackjack.py
@@ -2,8 +2,9 @@ import random
 
 SUITS = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
 RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
 class Card:
-    def __init__(self,rank,suit,id):
+    def __init__(self,rank,suit,id=None):
         self.rank = rank
         self.suit = suit
         self.id = id
@@ -22,9 +23,6 @@ class Card:
     def is_ace(self):
         return self.rank == 'A'
 
-
-
-
 class Hand:
     def __init__(self):
         self.cards = []
@@ -34,7 +32,8 @@ class Hand:
     def get_value(self):
         total = 0
         for i in self.cards:
-            total += i
+            val = i.point
+            total += val
         return total
     def get_soft_value(self):
         total = 0
@@ -45,18 +44,36 @@ class Hand:
                 total += i
         return total
 
-
-
-
-
 class Deck:
-
-   def __init__(self):
-       self.cards = []
-       for suit in SUITS:
-           for rank in RANKS:
-               self.cards.append(rank,suit)
+    def __init__(self):
+        self.cards = [Card(rank, suit) for rank in RANKS for suit in SUITS]
+        self.shuffle()
     def shuffle(self):
-        return random.shuffle(self.cards)
+        random.shuffle(self.cards)
+    def deal_card(self):
+        card = self.cards.pop()
+        return card
+
 class Play:
+    myDeck = Deck()
+    PlayerHand = Hand()
+    DealerHand = Hand()
+    in_play = True
+    PlayerHand.add_card(myDeck.deal_card())
+    DealerHand.add_card(myDeck.deal_card())
+    PlayerHand.add_card(myDeck.deal_card())
+    DealerHand.add_card(myDeck.deal_card())
+    DealerHand.add_card(myDeck.deal_card())
+    #while in_play:
+    if (DealerHand.get_value() or PlayerHand.get_value()) > 21:
+        print("You Lose!")
+        print(PlayerHand.get_value())
+        print(DealerHand.get_value())
+    else:
+        print("Test")
+        print(PlayerHand.get_value())
+        print(DealerHand.get_value())
+
+
+
 
